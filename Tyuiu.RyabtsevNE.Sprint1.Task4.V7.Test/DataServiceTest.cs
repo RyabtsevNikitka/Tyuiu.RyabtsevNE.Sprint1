c215c01e44ec5170c5898dd00a4fe443d53b8491
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-09-29 13:21:31 +0500

    Task4

--- Tyuiu.RyabtsevNE.Sprint1.Task4.V7.Test/DataServiceTest.cs
+++ Tyuiu.RyabtsevNE.Sprint1.Task4.V7.Test/DataServiceTest.cs
@@ -10,8 +10,8 @@ namespace Tyuiu.RyabtsevNE.Sprint1.Task4.V7.Test
         {
             DataService ds = new DataService();
             double x = 2.0;
-            double y = 4.0;
-            double wait = 0.038;
+            double y = 2.0;
+            double wait = 1.188;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
