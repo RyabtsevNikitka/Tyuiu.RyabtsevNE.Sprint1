c059f602dff2db9659821a58c511b03d8e1b3f14
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-09-29 11:40:16 +0500

    Task2

---
+++ Tyuiu.RyabtsevNE.Sprint1.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.RyabtsevNE.Sprint1.Task2.V11.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint1.Task2.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ds = new DataService();
+            int valu1 = 1;
+            int valu2 = 20;
+            int res = ds.ConvertHoursMinutesToSeconds(valu1, valu2);
+            Assert.AreEqual(4800, res);
+        }
+    }
+}
