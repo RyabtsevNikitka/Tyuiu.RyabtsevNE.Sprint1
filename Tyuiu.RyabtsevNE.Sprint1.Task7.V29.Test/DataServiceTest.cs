d8d755f7f2b5f3b8f0db8481572a4302e1ab0ba0
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-10-01 18:53:19 +0500

    Task7

---
+++ Tyuiu.RyabtsevNE.Sprint1.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.RyabtsevNE.Sprint1.Task7.V29.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint1.Task7.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 2.0;
+            double wait = 1.188;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
